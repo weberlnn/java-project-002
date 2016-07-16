a8ec7b077a4cc0b161f671b085aada382de54999
unknown <>
2016-07-16 19:27:14 +0800

    第一次提交

---
+++ src/Example.java
@@ -0,0 +1,19 @@
+
+public class Example{
+    public static void main(String args[]) {
+        double dist;
+        dist = 3.2 / 2 * 1100;
+        System.out.println("岩壁距离是"+dist+"英尺");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
